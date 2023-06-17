e1d650ae869fbf6b944ccf05a5f3fc4252961cf1
GabrielOmuro <gabriel_omuro@estudante.sesisenai.org.br>
2023-06-17 15:13:53 -0300

    Correção de alguns bugs

--- src/formulários/cadUsuario.jsx
+++ src/formulários/cadUsuario.jsx
@@ -1,13 +1,20 @@
+import './cadUsuario.css'
+import { useNavigate } from "react-router-dom"
+import { useState } from 'react'
+
 function CadUsuario() {
+    const navigate = useNavigate()
+    const [email, setEmail] = useState('')
+    const [senha, setSenha] = useState('')
 
     return (
         <div className="cadUsuarioContainer">
             <h2>Cadastre-se</h2><br />
             <form>
                 <label htmlFor="email">Email:</label>
-                <input type="email"/>
+                <input type="email" onChange={(event) => setEmail(event.target.value)}/>
                 <label htmlFor="senha">Senha:</label>
-                <input type="password"/>
+                <input type="password" onChange={(event) => setSenha(event.target.value)}/>
                 <button type="submit"  onClick={() => {if(email === ''){
                      alert('AVISO: Preencha o campo de email!')
                      return false
@@ -15,7 +22,7 @@ function CadUsuario() {
                         alert('AVISO: Preencha o campo de senha!')
                         return false
                      }else{
-                        alert('AVISO: Login efetuado com sucesso!')
+                        alert('AVISO: Cadastro efetuado com sucesso!')
                         navigate("/login")
                      }
                      }}>Cadastrar</button>
