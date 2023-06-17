19cb788ae683bdbfefd0159330ab3789f740c0fb
GabrielOmuro <gabriel_omuro@estudante.sesisenai.org.br>
2023-06-16 21:03:50 -0300

    Criação do arquivo footer

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -3,6 +3,9 @@ import './Footer.css'
 function Footer() {
     return (
         <div className="Footer">
+            <footer>
+                <p>Sistema desenvolvido por Gabriel Akira Omuro | https://github.com/GabrielOmuro | https://www.linkedin.com/in/gabriel-akira-omuro-122352264/</p>
+            </footer>
         </div>
     )
 }
